ddd7bc234a2470a14d3646aa678cb15799d69414
Bailey Townsend <baileytownsend2323@gmail.com>
2025-03-27 21:28:58 -0500

    Decent base

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,75 @@
-use anyhow::Result;
-use embedded_graphics::{
-    image::Image,
-    mono_font::{MonoTextStyle, ascii::FONT_6X9},
-    pixelcolor::BinaryColor,
-    prelude::*,
-    text::Text,
-};
+use anyhow::{Ok, Result};
+use clap::Parser;
+use embedded_graphics::{image::Image, pixelcolor::BinaryColor, prelude::*};
 use embedded_graphics_simulator::{
     BinaryColorTheme, OutputSettingsBuilder, SimulatorDisplay, Window,
 };
+use env_logger::Env;
+use log::info;
 use tinybmp::Bmp;
-// use std::sync::{Arc, Mutex};
-use tokio::sync::Mutex;
-type TRMNLWindow = Mutex<Window>;
+use tokio::sync::mpsc::{self, Sender};
+
+mod models;
+
+/// Setup arguments
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct ApiArguments {
+    /// Your API key found in TRMNL's developer settings
+    #[arg(short, long)]
+    api_key: String,
+
+    /// Base url for the API
+    #[arg(short, long, default_value = "https://usetrmnl.com")]
+    base_url: String,
+}
+
+enum Message<'a> {
+    NewImage(Bmp<'a, BinaryColor>),
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    println!("got value from the server; result");
-    let mut display = SimulatorDisplay::<BinaryColor>::new(Size::new(800, 480));
-    let text_style = MonoTextStyle::new(&FONT_6X9, BinaryColor::On);
+    let env = Env::default().filter_or("RUST_LOG", "info");
+    env_logger::init_from_env(env);
 
-    Text::new("Hello World!", Point::new(5, 5), text_style).draw(&mut display)?;
+    let (tx, mut rx) = mpsc::channel::<Message>(1);
+    tokio::spawn(async move {
+        web_calls(tx).await.unwrap();
+    });
 
     let output_settings = OutputSettingsBuilder::new()
         .scale(1)
         .theme(BinaryColorTheme::Default)
         .build();
-    // let mut window = ;
-    let window = Mutex::new(Window::new("TRMNL", &output_settings));
-    run_window(window).await.unwrap();
-    // tokio::spawn(async move {
-    //     run_window(window).await.unwrap();
-    // })
-    // .await
-    // .unwrap();
-    // window.show_static(&display);
+    let mut window = Window::new("TRMNL", &output_settings);
+    let mut display = SimulatorDisplay::<BinaryColor>::new(Size::new(800, 480));
 
-    // for event in window.events() {
-    //     println!("{:?}", event);
-    // }
+    loop {
+        //Waits for a new message
+        let message = rx.recv().await;
+
+        match message {
+            Some(message) => match message {
+                Message::NewImage(bmp) => {
+                    info!("New display update received");
+                    Image::new(&bmp, Point::zero()).draw(&mut display)?;
+                    window.show_static(&display);
+                }
+            },
+            None => {
+                info!("Channel has been closed, can end the process");
+                break;
+            }
+        }
+    }
 
     Ok(())
 }
 
-async fn run_window(window: TRMNLWindow) -> Result<()> {
-    let mut display = SimulatorDisplay::<BinaryColor>::new(Size::new(800, 480));
-    let text_style = MonoTextStyle::new(&FONT_6X9, BinaryColor::On);
-
-    // Text::new("Hello World!", Point::new(5, 5), text_style).draw(&mut display)?;
-
+async fn web_calls(sender: Sender<Message<'_>>) -> Result<()> {
     let bmp_data = include_bytes!("../test/byod_error.bmp");
-    let bmp: Bmp<'_, BinaryColor> = Bmp::from_slice(bmp_data).unwrap();
-    Image::new(&bmp, Point::new(0, 0)).draw(&mut display)?;
-
-    let mut window = window.lock().await;
-    window.show_static(&display);
-
-    for event in window.events() {
-        println!("{:?}", event);
-    }
-
+    let bmp = Bmp::<BinaryColor>::from_slice(bmp_data).unwrap();
+    let _ = sender.send(Message::NewImage(bmp)).await;
     Ok(())
 }
---
+++ src/models.rs
@@ -0,0 +1,20 @@
+use serde::{Deserialize, Serialize};
+
+/// It looks like all responses return 200 ok and then use the status field.
+#[derive(Serialize, Deserialize)]
+pub struct DisplayResponse {
+    /// 0 looks to be a success
+    pub status: u16,
+    /// The bmp url
+    pub image_url: Option<String>,
+    /// Set from the plugin if not mistaken
+    pub filename: Option<String>,
+    /// How long till refresh in seconds I think?
+    pub refresh_rate: Option<u32>,
+    /// Seems to always be included
+    pub reset_firmware: bool,
+    pub update_firmware: Option<bool>,
+    pub firmware_url: Option<String>,
+    ///I think this is an enum so will swap over later when I learn more about the api
+    pub special_function: Option<String>,
+}
