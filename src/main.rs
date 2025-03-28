f10cbb3aa7f43e0b49c82644cbeff25491758a0c
Bailey Townsend <baileytownsend2323@gmail.com>
2025-03-28 15:32:32 -0500

    Some qol changes

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,22 @@ use anyhow::{Result, anyhow};
 use clap::Parser;
 use dotenv::dotenv;
 use embedded_graphics::{image::Image, pixelcolor::BinaryColor, prelude::*};
-use embedded_graphics_simulator::SimulatorEvent;
 use embedded_graphics_simulator::{
-    BinaryColorTheme, OutputSettingsBuilder, SimulatorDisplay, Window,
+    BinaryColorTheme, OutputSettingsBuilder, SimulatorDisplay, SimulatorEvent, Window,
+    sdl2::Keycode,
 };
 use env_logger::Env;
-use log::{error, info};
+use log::{debug, error, info};
 use models::DisplayResponse;
-use reqwest::Client;
-use reqwest::header;
+use reqwest::{Client, header};
 use std::time::Duration;
 use tinybmp::Bmp;
-use tokio::signal;
-use tokio::sync::mpsc::{self, Sender};
-use tokio::time::sleep;
+use tokio::{
+    signal,
+    sync::mpsc::{self, Sender},
+    time::sleep,
+};
+
 mod models;
 
 static APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
@@ -59,47 +61,46 @@ async fn main() -> Result<()> {
 
     // I think 1 will be fine for now, but I might need to increase this later
     let (tx, rx) = mpsc::channel::<Message>(5);
+    // Used to early bail a tokio::sleep in web_calls
+    let (early_timeout_bail_sender, early_timeout_bail_receiver) = mpsc::channel::<()>(1);
 
     tokio::spawn(async move {
-        let _ = web_calls(tx, args).await;
+        let _ = web_calls(tx, early_timeout_bail_receiver, args).await;
     });
 
-    //TODO still not working need to close window first to close
     tokio::select! {
         _ = signal::ctrl_c() => {
             info!("Ctrl-C received, shutting down");
-            // shutdown_tx.send(()).unwrap();
             return Ok(());
         },
-        // _ = web_calls(tx, args) => {},
-        _ = run_display(rx) => {},
+        _ = run_display(rx, early_timeout_bail_sender) => {},
     }
 
     Ok(())
 }
 
-async fn run_display(mut rx: mpsc::Receiver<Message>) -> Result<()> {
+async fn run_display(
+    mut rx: mpsc::Receiver<Message>,
+    early_timeout_bail: mpsc::Sender<()>,
+) -> Result<()> {
     let output_settings = OutputSettingsBuilder::new()
         .scale(1)
         .pixel_spacing(1)
         .theme(BinaryColorTheme::Default)
         .build();
     let mut window = Window::new("TRMNL", &output_settings);
+    //800x480
     let mut display = SimulatorDisplay::<BinaryColor>::new(Size::new(800, 480));
 
     loop {
         _ = match rx.try_recv() {
-            Ok(message) => {
-                // Handle the successful case here
-                info!("Successfully received a message");
-                match message {
-                    Message::NewImage(bmp_bytes) => {
-                        info!("New display update received");
-                        let bmp = Bmp::<BinaryColor>::from_slice(&bmp_bytes).unwrap();
-                        let _ = Image::new(&bmp, Point::zero()).draw(&mut display);
-                    }
+            Ok(message) => match message {
+                Message::NewImage(bmp_bytes) => {
+                    info!("New display update received");
+                    let bmp = Bmp::<BinaryColor>::from_slice(&bmp_bytes).unwrap();
+                    let _ = Image::new(&bmp, Point::zero()).draw(&mut display);
                 }
-            }
+            },
             Err(_) => {}
         };
 
@@ -107,21 +108,43 @@ async fn run_display(mut rx: mpsc::Receiver<Message>) -> Result<()> {
 
         for event in window.events() {
             match event {
-                SimulatorEvent::Quit => break,
-
+                SimulatorEvent::Quit => {
+                    return Ok(());
+                }
+                SimulatorEvent::KeyDown {
+                    keycode,
+                    keymod: _,
+                    repeat: _,
+                } => match keycode {
+                    Keycode::Return => {
+                        debug!("Return key pressed");
+                        let _ = early_timeout_bail.send(()).await;
+                    }
+                    _ => {
+                        debug!("Unhandled keycode: {:?}", keycode);
+                    }
+                },
                 _ => {}
             }
         }
-        sleep(Duration::from_millis(20)).await;
+        // Have to always update the display or it crashes. Faster fps (lower sleep) helps keep the process down
+        // Get to high enough and it will crash
+        // And if it's too high keypresses are missed
+        sleep(Duration::from_secs(1)).await;
     }
 }
 
-async fn web_calls(sender: Sender<Message>, config: ApiArguments) -> Result<()> {
+async fn web_calls(
+    sender: Sender<Message>,
+    mut early_timeout_bail: mpsc::Receiver<()>,
+    config: ApiArguments,
+) -> Result<()> {
     let mut headers = header::HeaderMap::new();
     headers.insert(
         "access-token",
         header::HeaderValue::from_str(&config.api_key)?,
     );
+
     let client = Client::builder()
         .user_agent(APP_USER_AGENT)
         .default_headers(headers)
@@ -132,7 +155,12 @@ async fn web_calls(sender: Sender<Message>, config: ApiArguments) -> Result<()> 
     loop {
         if !first_run {
             info!("Sleeping for {} seconds", sleep_time);
-            sleep(Duration::from_secs(sleep_time)).await;
+            tokio::select! {
+                _ = sleep(Duration::from_secs(sleep_time)) => {},
+                _ = early_timeout_bail.recv() => {
+                    info!("Refreshing now");
+                }
+            }
         } else {
             first_run = false;
         }
